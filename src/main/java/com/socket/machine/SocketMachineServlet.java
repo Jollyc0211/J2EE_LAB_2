5adbb4e6e062c23a5ee3e4ca645ad120c2e336fd
jollyc0211 <jollyc0211@gmail.com>
2025-01-18 23:37:20 -0500

    week2

--- src/main/java/com/socket/machine/SocketMachineServlet.java
+++ src/main/java/com/socket/machine/SocketMachineServlet.java
@@ -7,41 +7,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Servlet implementation class SocketMachineServlet
- */
+
 @WebServlet("/SocketMachineServlet")
 public class SocketMachineServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    
     public SocketMachineServlet() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		String socketType = request.getParameter("socket_type");
         String quantityStr = request.getParameter("quantity");
         String customerName = request.getParameter("customer_name");
         String customerEmail = request.getParameter("customer_email");
         String errorMessage = "";
+        
+        if (customerName == null || customerName.trim().isEmpty()) {
+            errorMessage = "Error: Customer name is missing or invalid";
+        }else if (!customerName.matches("^[a-zA-Z\\s]+$")) {
+            errorMessage = "Error: Invalid Customer name";
+        }
 
-        // Validate input
+        
         int quantity = -1;
         try {
             quantity = Integer.parseInt(quantityStr);
@@ -51,7 +49,7 @@ public class SocketMachineServlet extends HttpServlet {
         	errorMessage = "Error: Invalid quantity";
         }
 
-        // Calculate the total price based on socket type and quantity
+     
         double pricePerSocket = 0;
         if(socketType != null) {
         switch (socketType.toUpperCase()) {
